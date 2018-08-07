e923f9d0c465d0696b58d712cac5fe6057f0b9f9
Your Name <you@example.com>
2018-08-07 11:27:51 +0300

    added linear gradient

--- tools/makeTestimage.py
+++ tools/makeTestimage.py
@@ -11,9 +11,14 @@ checkerboard = np.array(
     dtype =np.float64
 )
 
+lin_gradient = np.tile(np.linspace(1000, 2000, data.shape[0]), (data.shape[1], 1)).T
+
 pattern = np.zeros((32, 32))
-pattern[0,0] = 1
+pattern[15, 15] = 1
+
 data.data = np.tile(pattern, (data.shape[0]//pattern.shape[0], data.shape[1]//pattern.shape[1]))
+data.data = data.data * lin_gradient
+
 
 #imshow(data)
 #show()
