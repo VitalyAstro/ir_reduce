bbb566522f6788640c20d39c73e518abf39c92c5
Sebastian <sebastian.messlinger@posteo.de>
2018-09-10 10:23:48 +0300

    sketch of variable detection

--- scratch/scampTrial/ldac.py
+++ scratch/scampTrial/ldac.py
@@ -91,3 +91,17 @@ def get_table_from_ldac(filename, frame=1):
         frame = frame*2
     tbl = Table.read(filename, hdu=frame)
     return tbl
+
+# run sextractor, scamp
+# Need MAG_BEST, MAGERR_BEST in sex.config and SAVE_REFCATALOG in scamp.config
+
+# read scamp output catalog, header -> wcs and sextractor output cat
+
+scamp_header = fits.Header.fromtextfile('sexout.head')
+wcs=astropy.wcs.WCS(scamp_header)
+gaia=get_table_from_ldac('GAIA-DR1_1915+0719_r4.cat')
+sex = get_table_from_ldac('sexout.fits')
+# hacky way to go over pandas -> else we get a structured array that world2pix cannot handle
+# origin does not seem to really matter...
+gaia_px=wcs.all_world2pix(np.array(gaia['X_WORLD','Y_WORLD'].to_pandas()),1)
+
