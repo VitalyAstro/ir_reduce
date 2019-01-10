af2ccf226553d6b47e32e8cadd19180f06a97188
Sebastian <sebastian.messlinger@posteo.de>
2019-01-10 13:31:52 +0200

    adapted trial script for transient detection to new astropy version

--- scratch/scampTrial/check_transients.py
+++ scratch/scampTrial/check_transients.py
@@ -1,18 +1,28 @@
-from ldac import *
-%pylab
+from pylab import *
+
+import astropy
+import astropy.wcs
+import astropy.io
+import astropy.table
+
 # run sextractor, scamp
 # Need MAG_BEST, MAGERR_BEST in sex.config and SAVE_REFCATALOG in scamp.config
 
 # read scamp output catalog, header -> wcs and sextractor output cat
 
-scamp_header = fits.Header.fromtextfile('sexout.head')
+scamp_header = astropy.io.fits.Header.fromtextfile('sexout.head')
 wcs=astropy.wcs.WCS(scamp_header)
-gaia=get_table_from_ldac('GAIA-DR1_1915+0719_r4.cat')
-sex = get_table_from_ldac('sexout.fits')
+gaia=astropy.table.Table(astropy.io.fits.open('GAIA-DR1_1915+0719_r4.cat')[2].data) # third hdu, get the numpy recarray, convert to astropy table
+
+sex = astropy.table.Table(astropy.io.fits.open('sexout.fits')[2].data)
 # hacky way to go over pandas -> else we get a structured array that world2pix cannot handle
 # origin does not seem to really matter...
 gaia_px=wcs.all_world2pix(np.array(gaia['X_WORLD','Y_WORLD'].to_pandas()),1)
 sex_world=wcs.wcs_pix2world(np.array(sex['XWIN_IMAGE','YWIN_IMAGE'].to_pandas()),1)
 
+# Do calculations in image coordinates so we can use error to estimate same-ness of detections
+
 plt.scatter(gaia['X_WORLD'], gaia['Y_WORLD'])
 plt.scatter(sex_world[:, 0], sex_world[:, 1])
+
+plt.show()
